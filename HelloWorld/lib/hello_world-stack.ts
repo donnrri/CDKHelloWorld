f8bf4ac7cefb81af877b8027ce3fdbf118cf13e9
davet <daveenq@gmail.com>
2021-02-28 11:10:22 +0000

    Create a cluster for our fargate task and add to created vpc

--- HelloWorld/lib/hello_world-stack.ts
+++ HelloWorld/lib/hello_world-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
 
 export class HelloWorldStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -7,6 +8,10 @@ export class HelloWorldStack extends cdk.Stack {
 
     const vpc = new ec2.Vpc(this, 'HelloWorldVPC', {
       maxAzs: 3
+    })
+
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      vpc: vpc
     })
 
   }
