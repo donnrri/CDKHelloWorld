dadffcf8ac983eac7fe8f1fef011f0fc4e2f0d91
davet <daveenq@gmail.com>
2021-02-28 12:22:29 +0000

    Added code to use an exisying VPC if one is available, pass in via env vars

--- HelloWorld/lib/hello_world-stack.ts
+++ HelloWorld/lib/hello_world-stack.ts
@@ -1,17 +1,25 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs from '@aws-cdk/aws-ecs';
+import { IVpc } from '@aws-cdk/aws-ec2';
 import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 import * as path from 'path';
+
 
 export class HelloWorldStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'DTHelloWorldVPC', {
-      maxAzs: 3
-    })
+    let vpc:IVpc
 
+    if(process.env.VPC_ID){
+      vpc = ec2.Vpc.fromLookup(this, process.env.VPC_ID, {})
+    }else{
+      vpc =  new ec2.Vpc(this, 'DTHelloWorldVPC', {
+        maxAzs: 3
+      })
+    }
+  
     const cluster = new ecs.Cluster(this, 'DTHelloWorldCluster', {
       vpc: vpc
     })
