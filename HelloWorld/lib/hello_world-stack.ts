be6be88b095dd22ec17ceb31aa1ffc454ac2b209
donnrri <dthorneycroft@gmail.com>
2021-03-02 16:52:02 +0000

    Refactored some of the stackcode

--- HelloWorld/lib/hello_world-stack.ts
+++ HelloWorld/lib/hello_world-stack.ts
@@ -6,9 +6,7 @@ import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 import * as route53 from '@aws-cdk/aws-route53'
 import * as certmanager from '@aws-cdk/aws-certificatemanager';
 import * as cognito from "@aws-cdk/aws-cognito";
-
 import * as path from 'path';
-import { hostname } from 'os';
 
 
 export class HelloWorldStack extends cdk.Stack {
@@ -16,21 +14,7 @@ export class HelloWorldStack extends cdk.Stack {
     super(scope, id, props);
 
     let vpc:IVpc
-
-    const hostedZone = new route53.HostedZone(this, "HostedZone",{
-        zoneName: process.env.DOMAIN_NAME
-      }
-     )
-  
-    const cert = new certmanager.DnsValidatedCertificate(
-      this,
-      "Certificate",
-      {      
-        hostedZone: hostedZone,
-        domainName: process.env.DOMAIN_NAME
-      }
-    )
-
+    
     const pool = new cognito.UserPool(this, "DTHelloWorlUserPool", {
       selfSignUpEnabled: true,
       userPoolName:"DTHelloWorlUserPool"
@@ -40,18 +24,18 @@ export class HelloWorldStack extends cdk.Stack {
       this, 
       "DTHelloWorldCustomDomain",
       {
-        domain:process.env.USERPOOL_DOMAIN,
-        userPoolId: pool.userPoolId,
+        domain: 'hello-world-domain',
+        userPoolId: pool.userPoolId
 
       }
     )
 
-    const userpoolClient = new cognito.UserPoolClient( this, "DTHelloWorldUserPoolClient", {
+   
+    const userpoolClient = new cognito.UserPoolClient( this, "DTHelloWorldUserPoolClinet", {
       generateSecret: true,
       userPool: pool,
-      userPoolClientName: "DTHelloWorldUserPoolClient"
+      userPoolClientName: "DTHelloWorldUserPoolClinet"
     })
-
 
     if(process.env.VPC_ID != null){
       vpc = ec2.Vpc.fromLookup(this, process.env.VPC_ID, {})
@@ -61,22 +45,56 @@ export class HelloWorldStack extends cdk.Stack {
       })
     }
   
+    const hostedZone = route53.HostedZone.fromLookup(this, "HostedZone",{
+      domainName: process.env.DOMAIN_NAME,
+      privateZone: false
+      }
+     )
+
+   const certificate = new certmanager.DnsValidatedCertificate(
+    this,
+    "certificate",{
+      hostedZone: hostedZone,
+      domainName:  process.env.DOMAIN_NAME,
+      region: process.env.REGION
+    }
+)
+
     const cluster = new ecs.Cluster(this, 'DTHelloWorldCluster', {
       vpc: vpc
     })
 
-    new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'DTHelloWorldFargateService', {
+  const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'DTHelloWorldFargateService', {
       cluster: cluster,
-      certificate: cert,
-      domainName: process.env.DOMAIN_NAME,
-      domainZone: hostedZone,
+      domainName:  process.env.DOMAIN_NAME',
+      domainZone:hostedZone,
+      certificate: certificate,
       cpu: 512,
       desiredCount: 3,
-      taskImageOptions: {image: ecs.ContainerImage.fromAsset(path.resolve(__dirname, '..','container'))},
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromAsset(path.resolve(__dirname, '..','container')),
+      },
       memoryLimitMiB: 2048,
-      publicLoadBalancer: true
+      publicLoadBalancer: true,
+      assignPublicIp: true
     })
 
+    fargateService.targetGroup.configureHealthCheck({
+      enabled: true,
+      path:'/healthcheck',
+      healthyHttpCodes:'200'
+      }
+    )
+
+    const securityGroup = fargateService.loadBalancer.connections.securityGroups[0]
+
+    securityGroup.addEgressRule(ec2.Peer.anyIpv4(), new ec2.Port({
+      protocol: ec2.Protocol.TCP,
+      stringRepresentation:"443",
+      fromPort: 443, 
+      toPort: 443
+    })
+    )
   }
   
 }
